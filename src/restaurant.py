a24e4591fd269b99f5ef81b0b25007153d7b65f0
Jacob Park <jacobypark122@gmail.com>
2023-03-21 16:04:40 -0400

    done

--- src/restaurant.py
+++ src/restaurant.py
@@ -1,2 +1,13 @@
 class Restaurant:
-    pass
+    def __init__(self,name):
+        self.name = name
+        self.reviews = []
+
+    def get_name(self):
+        return self.name
+    
+    def get_reviews(self):
+        return self.reviews
+    
+    def average_rating(self):
+        return sum(self.reviews.rating) / len(self.reviews)
